0ed956614eb5f354ca25870424d19cfc907e589a
firmo holanda <firmo@viize.com>
2020-01-28 18:09:58 -0300

    correct rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 60 }
   validates :email, presence: true, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-
 end
